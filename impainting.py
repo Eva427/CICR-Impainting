bd496af610b6ba96ed8cb6bda1a67fe75953100f
HeineKayn <thomashoger@hotmail.fr>
2022-06-08 15:27:50 +0200

    auto-encode

--- impainting.py
+++ impainting.py
@@ -1,3 +1,3 @@
 from impaintingLib import *
 
-o = Obj()
+o = AutoEncoder()
--- impaintingLib/model.py
+++ impaintingLib/model.py
@@ -1,4 +1,35 @@
+import torch.nn as nn
+
+# simple auto encoder
+class AutoEncoder(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            nn.ReLU(True)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        z = self.encoder(x)
+        y = self.decoder(z)
+        return y
+# uNet
+
 # partial convolution
 # pixel shuffle 
-# simple auto encoder
-# uNet
