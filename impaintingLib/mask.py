c11cc1a37c59ab2e3850495b1e9d5321ab6581cf
Thomas <hoger@insa-toulouse.fr>
2022-06-20 11:29:55 +0000

    extern cutouts

--- impaintingLib/mask.py
+++ impaintingLib/mask.py
@@ -9,7 +9,7 @@ class Alter :
         self.seed    = seed
 
     # Generate square mask
-    def squareMask(self,imgs):
+    def squareMaskDirectly(self,imgs):
         
         np.random.seed(self.seed)
         
@@ -26,6 +26,26 @@ class Alter :
             cut_img[:, h11:h11 + h11, w11:w11 + w11] = 0
             cut_img[:, h22:h22 + h11, w22:w22 + w22] = 0
             cutouts[i] = cut_img
+        return cutouts
+    
+    # Generate square mask
+    def squareMask(self,imgs):
+        
+        np.random.seed(self.seed)
+        
+        n, c, h, w = imgs.shape
+        w1 = np.random.randint(self.min_cut, self.max_cut, n)
+        h1 = np.random.randint(self.min_cut, self.max_cut, n)
+        
+        w2 = np.random.randint(self.min_cut, self.max_cut, n)
+        h2 = np.random.randint(self.min_cut, self.max_cut, n)
+        
+        cutouts = torch.empty((n, 4, h, w), dtype=imgs.dtype, device=imgs.device)
+        for i, (img, w11, h11, w22, h22) in enumerate(zip(imgs, w1, h1, w2, h2)):
+            cut_img = torch.full((1,h,w),255, dtype=img.dtype, device=img.device)
+            cut_img[:, h11:h11 + h11, w11:w11 + w11] = 0
+            cut_img[:, h22:h22 + h11, w22:w22 + w22] = 0
+            cutouts[i] = torch.cat((img,cut_img),0)
         return cutouts
     
     def none(self):
