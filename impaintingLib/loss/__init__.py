2caae255e4241755a28e9c03a3a80c17cfaae81d
HeineKayn <thomashoger@hotmail.fr>
2022-07-20 14:23:20 +0200

    enhance

--- impaintingLib/loss/__init__.py
+++ impaintingLib/loss/__init__.py
@@ -2,3 +2,4 @@ from .common import totalVariation
 from .perceptualVGG import perceptualVGG
 from .perceptualAE import perceptualAE
 from .perceptualClassifier import perceptualClassifier,getTrainedModel
+from .classifierUtils import generate_label, generate_label_plain
--- test.py
+++ test.py
@@ -16,16 +16,18 @@ models[0].load_state_dict(torch.load('./modelSave/partial_22channels_highres.pth
 
 # resize = (64,64)
 # resize = (192,192)
+# resize = (128,128)
 resize = (256,256)
 
-def classify(x):
+def classify(x,w):
     xNormalized = imp.data.normalize(x)
     modelPath = "./modelSave/classifierUNet.pth"
     classif = imp.model.ClassifierUNet()
     classif.load_state_dict(torch.load(modelPath,map_location=torch.device('cpu')))
     normalized = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(x)
     classifiedImage = classif(normalized)
-    return torch.cat((xNormalized,classifiedImage),dim=1)
+    colors = imp.loss.generate_label(classifiedImage,w)
+    return torch.cat((xNormalized,classifiedImage),dim=1),colors
 
 def convertImage(input):
     image, mask = input["image"], input["mask"]
@@ -43,7 +45,7 @@ def convertImage(input):
     c,w,h = image.shape
     image = image.view(1,c,w,h)
     #image = imp.data.normalize(image)
-    image = classify(image)
+    image,colors = classify(image,w)
 
     mask  = mask[:1]
     image = imp.mask.propagate(image,mask)
@@ -51,7 +53,7 @@ def convertImage(input):
     # alter = imp.mask.Alter(min_cut=2, max_cut=25)
     # image = alter.squareMask(image)
 
-    return image
+    return image,colors
 
 def predict(image):
     for model in models:
@@ -63,10 +65,19 @@ def predict(image):
     image = torch.clip(image,0,1)
     return image[:,:3]
 
+def enhance(image):
+    image = transforms.Resize((64,64))(image)
+    model_path = 'modelSave/RRDB_ESRGAN_x4.pth'
+    model = imp.model.RRDBNet(3, 3, 64, 23, gc=32)
+    model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
+    return model(image)
+
 def impaint(input):
-    image_prime = convertImage(input)
+    image_prime,colors = convertImage(input)
     image_hat = predict(image_prime)
+    #image_hat = enhance(image_hat)
     image_hat = transforms.ToPILImage()(image_hat[0])
+    colors = transforms.ToPILImage()(colors[0])
 
     # image_prime = imp.data.inv_normalize(image_prime)
     # image_prime = torch.clip(image_prime,0,1)
@@ -74,7 +85,7 @@ def impaint(input):
     # image_prime.show()
     # image_hat.show()
     
-    return image_hat
+    return image_hat,colors
 
 css = ".output_image {height: 40rem !important; width: 100% !important;}"
 interface = gr.Blocks(css=css)
@@ -88,11 +99,13 @@ with interface:
             with gr.Row():
                 btn = gr.Button("Run")
         with gr.Column():
+            img3 = gr.Image(interactive=True, label="Classifer")
+            html = gr.HTML(visible=False, value="<img src='file/linkdin.jpg' width='200px'>")
+        with gr.Column():
             img2 = gr.Image(interactive=True, label="Output")
-            #img3 = gr.Image()
 
-    btn.click(fn=impaint, inputs=img, outputs=img2)
-    #btn.click(fn=fn, inputs=img, outputs=[img2, img3])
+    #btn.click(fn=impaint, inputs=img, outputs=img2)
+    btn.click(fn=impaint, inputs=img, outputs=[img2, img3])
 
 
 interface.launch()
