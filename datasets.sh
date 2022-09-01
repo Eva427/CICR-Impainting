0aa0b95bbd8c2491c2a5d8cf7ea02c1a839a9068
Thomas <hoger@insa-toulouse.fr>
2022-09-01 13:31:14 +0000

    bash change

--- datasets.sh
+++ datasets.sh
@@ -5,25 +5,28 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 unzip celeba.zip -d data/celeba
 
 echo 'Downloading flickr dataset ... '
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tg-Ur7d4vk1T8Bn0pPpUSQPxlPGBlGfv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1tg-Ur7d4vk1T8Bn0pPpUSQPxlPGBlGfv" -O flickr.zip && rm -rf /tmp/cookies.txt
-unzip flickr.zip -d data/flickr
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bgffRt1JPRv5k3vmF-NEATLkiPmq_e8w' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bgffRt1JPRv5k3vmF-NEATLkiPmq_e8w" -O flickr.zip && rm -rf /tmp/cookies.txt
+unzip flickr.zip 
+mkdir -p data/flickr 
+mv thumbnails128x128 data/flickr 
 
 echo 'Downloading lfw dataset ... '
 wget http://vis-www.cs.umass.edu/lfw/lfw.tgz 
 tar zxvf lfw.tgz 
-mkdir data 
 mv lfw data 
 
 echo 'Downloading utk dataset ... '
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0BxYys69jI14kU0I1YUQyY1ZDRUE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0BxYys69jI14kU0I1YUQyY1ZDRUE" -O utk.zip && rm -rf /tmp/cookies.txt
-unzip utk.zip -d data/utk
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0BxYys69jI14kYVM3aVhKS1VhRUk' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0BxYys69jI14kYVM3aVhKS1VhRUk" -O utk.tar.gz && rm -rf /tmp/cookies.txt
+tar xvzf utk.tar.gz
+mkdir -p data/utkface/utkface
+mv UTKFace/ data/utkface/utkface
 
 echo 'Downloading nvidia mask testing dataset ... '
 wget https://www.dropbox.com/s/01dfayns9s0kevy/test_mask.zip
 unzip test_mask.zip
-mv mask test_masks
+mv mask data/test_masks
 
 echo 'Downloading nvidia mask training dataset ... '
 wget https://www.dropbox.com/s/qp8cxqttta4zi70/irregular_mask.zip
 unzip irregular_mask.zip
-mv irregular_mask mask
+mv irregular_mask data/mask
